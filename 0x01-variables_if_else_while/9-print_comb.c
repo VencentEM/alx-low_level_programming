4c6692be79e3c2f2397846313c0b9df1aa673398
emy <luxestore1997@gmail.com>
2023-07-16 16:09:11 -0700

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,34 +3,23 @@
 /**
  * main - entry point
  *
- * Descption: print allpossible different
- * combination of two digit
+ * Descption: print 0 , 1,- 9
  *
  * Return: 0(success)
  */
 int main(void)
 {
-	int digit1 = 0, digit2;
+	int digit = 0;
 
-	while (digit1 <= 9)
+	while (digit <= 9)
 	{
-		digit2 = 0;
-		while (digit2 <= 9)
+		putchar(digit + 48);
+		if (digit != 9)
 		{
-			if (digit1 != digit2 && digit1 < digit2)
-			{
-				putchar(digit1 + 48);
-				putchar(digit2 + 48);
-
-				if (digit1 + digit2 != 17)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-			digit2++;
+			putchar(',');
+			putchar(' ');
 		}
-		digit1++;
+		digit++;
 	}
 	putchar('\n');
 
