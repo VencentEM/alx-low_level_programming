3bee944e7bf61ab85be925ecce8e94f13929b4c1
emy <luxestore1997@gmail.com>
2023-07-24 16:08:44 -0700

    task 0

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -19,8 +19,8 @@ void print_diagonal(int n)
 	{
 		for (a = 0; a < n; a++)
 		{
-		for (b = 0; b < a; b++)
-			_putchar(' ');
+			for (b = 0; b < a; b++)
+				_putchar('');
 			_putchar('\');
 			_putchar('\n');
 		}
