93e0cdc0d151d17a8c8f38fe97dba2e4009ac26c
emy <luxestore1997@gmail.com>
2023-07-25 02:37:26 -0700

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /**
  *
- * reset_to_98 - takes a pointer to an int parameter
+ * reset_to_98.c - takes a pointer to an int parameter
  * and update the value it point to 98
  *
  * @n: int paramater
