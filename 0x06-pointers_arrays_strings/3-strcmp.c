adb42c1dbc3caa7b5bf580e996518dca6addf626
emy <luxestore1997@gmail.com>
2023-07-26 15:32:40 -0700

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,13 +1,24 @@
-#include <unistd.h>
+#include <main.h>
 
 /**
- * _putchar - writes the characters c to stdout
- * @c: the character to print
- *
- * Return: On success 1.
- * On error, -1 is returned and errnois set appropriately
+ * _strcmp - compare two strings
+ * @s1: string1
+ * @S2: string2
+ * Return: 1 if true, 0 if false
  */
-int _putchar(char c)
+int _strcmp(char *s1, char *s2)
 {
-	return (write(1, &c, 1));
+	int equal = 0;
+
+	while (*s1)
+	{
+	 	if (*s1 != *s2)
+		{
+			equal = ((int)*s1 - 48) - ((int)*s2 - 48);
+			break
+		}
+		s1++;
+		s2++;
+	}
+	return (equal);
 }
