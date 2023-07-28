acae225068d4604cc5538162078706671cb79fe8
emy <luxestore1997@gmail.com>
2023-07-28 15:51:17 -0700

    task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
- * *string_toupper - changes all lovercase letters of a string to upper
- * 
+ * string_toupper - changes all lovercase letters of a string to upper
  * @str: string to return
  * Return: string
  */
-char *string_toupper(char *)
+char *string_toupper(char *str)
 {
 	int i;
 
