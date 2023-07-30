11b0ece6cabd51dd66a40d5fe2f76cda7ff61b95
emy <luxestore1997@gmail.com>
2023-07-30 15:11:36 -0700

    task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,13 +1,41 @@
-#include <unistd.h>
-
-/**
- * _putchar - writes the characters c to stdout
- * @c: the character to print
- *
- * Return: On success 1.
- * On error, -1 is returned and errnois set appropriately
+#include "main.h"
+/**  
+ * rot13 - that encodes a string using rot13
+ * @chars:   
+ * Return: s
  */
-int _putchar(char c)
+
+char rot13(chars)
 {
-	return (write(1, &c, 1));
+	 char fh[] = {'A', 'B', 'C', 'D', 'E', 'F',
+		 'G', 'H', 'I', 'J', 'K', 'L',
+		 'M', 'N', 'O', 'P', 'Q', 'R',
+		 'S', 'T', 'U', 'V', 'W', 'X',
+		 'Y', 'Z', 'a', 'b', 'c', 'd',
+		 'e', 'f', 'g', 'h', 'i', 'j',
+		 'k', 'l', 'm', 'n', 'o', 'p',
+		 'q', 'r', 's', 't', 'u', 'v',
+		 'w', 'x', 'y', 'z'};
+	 char sh[] = {'N', 'O', 'P', 'Q', 'R', 'S',
+		  'T', 'U', 'V', 'W', 'X', 'Y',
+		  'Z', 'A', 'B', 'C', 'D', 'E',
+		  'F', 'G', 'H', 'I', 'J', 'K',
+		  'L', 'M', 'n', 'o', 'p', 'q',
+		  'r', 's', 't', 'u', 'v', 'w',
+		  'x', 'y', 'z', 'a', 'b', 'c',
+		  'd', 'e', 'f', 'g', 'h', 'i',
+		  'j', 'k', 'l', 'm'};
+	 unsigned int i, j;
+	 for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < sizeof(fh) / sizeof(char); j++)
+		{
+			if (s[i] == fh[j])
+			{
+				s[i] = sh[j];
+				break;
+			}
+		}
+	}
+	return (s);
 }
