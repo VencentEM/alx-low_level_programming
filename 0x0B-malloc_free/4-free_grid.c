7a42f6a38037e35d57f05dd1055f053eb108f1a3
emy <luxestore1997@gmail.com>
2023-08-07 05:31:38 -0700

    task 4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,21 +1,17 @@
 #include "main.h"
 
 /**
- * *create_array - creates an array of chars,
- * and initialize it with a specific char.
- * @size: size of the array
- * @c: char to initialize
- * Return: pointer to the array initialized or NULL
+ * free_grid - free the grid
+ * @grid: grid of memories
+ * @height: int
+ * Return: void
  */
 
-char *create_array(unsigned int size, char c)
+void free_grid(int **grid, int height)
 {
-	char *n = malloc(size);
+	int i = 0;
 
-	if (size == 0 || n == 0)
-		return (0);
-	while (size--)
-		n[size] = c;
-
-	return (n);
+	for (; i < height; i++)
+		free(grid[i]);
+	free(grid);
 }
