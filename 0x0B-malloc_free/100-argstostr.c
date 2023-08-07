ce90cf703d4ecc27f39cdc7f1f5d3f182dae3f7a
emy <luxestore1997@gmail.com>
2023-08-07 06:47:21 -0700

    task 5

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,21 +1,50 @@
 #include "main.h"
 
 /**
- * *create_array - creates an array of chars,
- * and initialize it with a specific char.
- * @size: size of the array
- * @c: char to initialize
- * Return: pointer to the array initialized or NULL
+ * _strlen - find length of a string
+ * @s: string
+ * Return: int
+ */
+int _strlen(char *s)
+{
+	int size = 0;
+
+	for (; s[size] != '\0'; size++)
+	;
+	return (size);
+}
+
+/**
+ * *argstostr - description
+ * @ac: int
+ * @av: arguments
+ * Return: string
  */
 
-char *create_array(unsigned int size, char c)
+char *argstostr(int ac, char **av)
 {
-	char *n = malloc(size);
+	int i = 0, nc = 0, j = 0, cmpt = 0;
+	char *s;
 
-	if (size == 0 || n == 0)
-		return (0);
-	while (size--)
-		n[size] = c;
+	if (ac == 0 || av == NULL)
+		return (NULL);
 
-	return (n);
+	for (; i < ac; i++, nc++)
+		nc += _strlen(av[i]);
+
+	s = malloc(sizeof(char) * nc + 1);
+	if (s == 0)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++, cmpt++)
+			s[cmpt] = av[i][j];
+
+		s[cmpt] = '\n';
+		cmpt++;
+
+	}
+	s[cmpt] = '\0';
+	return (s);
 }
