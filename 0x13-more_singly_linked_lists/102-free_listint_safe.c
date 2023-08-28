56c2d56339f16bb58517692e616b239e661aed6b
emy <luxestore1997@gmail.com>
2023-08-28 13:23:21 -0700

    task 102

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -6,7 +6,7 @@
  * _ra - reallocates memory for an array of pointers
  * to the nodes in a linked list
  * @list: the old list to append
- * @size: size of the new list 
+ * @size: size of the new list
  * @new: new node to add to the list
  *
  * Return: pointer to the new list
