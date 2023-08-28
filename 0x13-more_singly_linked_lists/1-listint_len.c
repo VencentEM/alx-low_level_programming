7c312eddd0243a2c74238abcd827cb66eb88294a
emy <luxestore1997@gmail.com>
2023-08-28 08:25:39 -0700

    task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * listint_len - prints a linked lists.
+ *
+ * @h: head pointer
+ *
+ * Return: size of list
+ *
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+
